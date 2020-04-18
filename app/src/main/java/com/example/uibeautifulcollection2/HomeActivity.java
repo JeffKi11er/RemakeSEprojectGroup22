cee12996e57865f4cf88807692300257cf4e8a9b
JeffKi11er <fillaytion23@gmail.com>
2020-04-18 23:58:55 +0700

    [check directory commit]

--- app/src/main/java/com/example/uibeautifulcollection2/HomeActivity.java
+++ app/src/main/java/com/example/uibeautifulcollection2/HomeActivity.java
@@ -1,11 +1,18 @@
 package com.example.uibeautifulcollection2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
-//    private
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+    private FrameLayout container;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
